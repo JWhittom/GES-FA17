6b6f1d9fbbf0b32c1ab65caf6ffeedc6cbf0ae75
Joshua Whittom <Joshua.Whittom@iam.local>
2017-10-23 18:21:39 -0500

    10/23 In Class
    Refined movement, added keys

---
+++ GES2DW1/Assets/Scripts/Key.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour {
+
+    static int keyCount = 0;
+    static int totalKeys = 0;
+    AudioSource audioSource;
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCol;
+    Text keyCountText;
+    private void Start()
+    {
+        totalKeys++;
+        keyCountText = GameObject.Find("KeyCountText").GetComponent<Text>();
+        UpdateText();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCol = GetComponent<BoxCollider2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            audioSource.Play();
+            keyCount++;
+            Debug.Log("Key " + keyCount + " Get!");
+            spriteRenderer.enabled = false;
+            boxCol.enabled = false;
+            UpdateText();
+            // Destroy(gameObject);
+        }
+    }
+
+    void UpdateText()
+    {
+        keyCountText.text = "Keys: " + keyCount + "/" + totalKeys;
+    }
+}
--- GES2DW1/Assets/Scripts/PlayerMovement.cs
+++ GES2DW1/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour {
     
     // Rigidbody for 2D Physics
     Rigidbody2D rb2D;
+    // Audio Source
+    AudioSource audioSource;
     // Is the player on the ground?
     bool isOnGround;
     // Horizontal input check
@@ -15,10 +17,16 @@ public class PlayerMovement : MonoBehaviour {
     bool shouldJump;
     // Jump force
     Vector2 jumpForce;
+    // Can the player grab?
+    bool canGrab;
     // Is the player grabbing?
     bool isGrabbing;
     // Grab joint
     HingeJoint2D grab;
+    // Object to grab
+    Rigidbody2D ropeRB;
+    // Ungrab Cooldown
+    float grabTime;
     // Movement speed
     [SerializeField]
     float moveSpeed = 5.0f;
@@ -34,6 +42,15 @@ public class PlayerMovement : MonoBehaviour {
     // Ground layer
     [SerializeField]
     LayerMask whatCountsAsGround;
+    // Center point for rope detection circle
+    [SerializeField]
+    Transform ropeCheck;
+    // Rope detection radius
+    [SerializeField]
+    float ropeRad = 0.55f;
+    // Rope layer
+    [SerializeField]
+    LayerMask countsAsRope;
 
     // Use this for initialization
     void Start()
@@ -41,6 +58,7 @@ public class PlayerMovement : MonoBehaviour {
         // this code teleports the game object
         //transform.position = new Vector3(0, 0, 0);
         rb2D = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -49,7 +67,9 @@ public class PlayerMovement : MonoBehaviour {
         jumpForce = new Vector2(0, jumpStrength);
         GetMovementInput();
         GetJumpInput();
-        Ungrab();
+        CanGrab();
+        Grab();
+        UnGrab();
         UpdateIsOnGround();
     }
 
@@ -76,20 +96,47 @@ public class PlayerMovement : MonoBehaviour {
         Jump();
     }
 
-    private void Ungrab()
+    private void CanGrab()
     {
-        if(Input.GetButtonDown("Jump") && isGrabbing)
+        Collider2D[] ropeObjects = Physics2D.OverlapCircleAll(ropeCheck.position, ropeRad, countsAsRope);
+        if(ropeObjects.Length > 0 && !isGrabbing)
         {
-            Destroy(grab);
-            isGrabbing = false;
+            canGrab = true;
+            ropeRB = ropeObjects[0].attachedRigidbody;
+        }
+        else
+        {
+            canGrab = false;
+            ropeRB = null;
         }
     }
 
+    private void Grab()
+    {
+        if(Input.GetButtonDown("Jump") && canGrab)
+        {
+            grab = gameObject.AddComponent<HingeJoint2D>();
+            grabTime = Time.time;
+            grab.connectedBody = ropeRB;
+            isGrabbing = true;
+            canGrab = false;
+        }
+    }
+
+    private void UnGrab()
+    {
+        if(Input.GetButtonDown("Jump") && isGrabbing && Time.time - grabTime > .5f)
+        {
+            Destroy(grab);
+            ropeRB = null;
+            isGrabbing = false;
+        }
+    }
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Rope")
         {
-            Debug.Log("Rope");
             if(!isGrabbing)
             {
                 grab = gameObject.AddComponent<HingeJoint2D>();
@@ -97,19 +144,24 @@ public class PlayerMovement : MonoBehaviour {
                 isGrabbing = true;
             }
         }
-    }
+    }*/
 
     // Checks for ground below and updates the isOnGround variable accordingly
     private void UpdateIsOnGround()
     {
         Collider2D[] groundObjects = Physics2D.OverlapCircleAll(groundDetectPoint.position, groundDetectRadius, whatCountsAsGround);
         isOnGround = groundObjects.Length > 0;
+        if (isGrabbing)
+            isOnGround = true;
     }
 
     // Player movement logic
     private void Move()
     {
-        rb2D.velocity = new Vector2(horizontalInput * moveSpeed, rb2D.velocity.y);
+        if (!isGrabbing)
+            rb2D.velocity = new Vector2(horizontalInput * moveSpeed, rb2D.velocity.y);
+        else
+            rb2D.AddForce(new Vector2(horizontalInput * moveSpeed * 2, 0));
         // Translate() doesn't use physics
         //transform.Translate(new Vector3(0.01f, 0, 0));
         // GetKey is difficult if things are going to change even a little bit
@@ -127,6 +179,7 @@ public class PlayerMovement : MonoBehaviour {
             // Don't use different/amalgamate systems for movement (it's like mixing battery types)
             //transform.translate(0, 1.0f, 0);
             //rb2D.velocity = new Vector2(rb2D.velocity.x, jumpStrength);
+            audioSource.Play();
             rb2D.AddForce(jumpForce, ForceMode2D.Impulse);
             isOnGround = false;
             shouldJump = false;
