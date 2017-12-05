57c27e196a622c8d4b1a317a2910b7ef113b3011
JWhittom <joshua.whittom@loop.colum.edu>
2017-12-04 18:19:22 -0600

    Classwork 12/4
    Backing up the scripts, etc. from class for inventory, reticle, and hover text

---
+++ ges-fa17-secton01-3d-project-master/ges-fa17-secton01-3d-project-master/Assets/Scripts/ActivateLookedAtObjects.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateLookedAtObjects : MonoBehaviour
+{
+    [SerializeField]
+    private float maxActivateDistance = 4.0f;
+
+    [SerializeField]
+    private Text hoverText;
+
+    private IActivatable objectLookedAt;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateObjectLookedAt();
+        UpdateHoverText();
+        HandleInput();
+    }
+
+    private void UpdateObjectLookedAt()
+    {
+        //Debug.DrawRay(transform.position, transform.forward * maxActivateDistance, Color.blue);
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(transform.position, transform.forward,
+            out raycastHit, maxActivateDistance))
+        {
+            //Debug.Log("Racast hit " + raycastHit.transform.name);
+
+            objectLookedAt = raycastHit.transform.GetComponent<IActivatable>();
+        }
+        else
+            objectLookedAt = null;
+    }
+
+    private void UpdateHoverText()
+    {
+        if (objectLookedAt == null)
+            hoverText.text = string.Empty;
+        else
+            hoverText.text = objectLookedAt.NameText;
+    }
+
+    private void HandleInput()
+    {
+        if (objectLookedAt != null && Input.GetButtonDown("Activate"))
+        {
+            objectLookedAt.DoActivate();
+        }
+    }
+}
---
+++ ges-fa17-secton01-3d-project-master/ges-fa17-secton01-3d-project-master/Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour, IActivatable
+{
+    [SerializeField]
+    private string nameText;
+
+    [SerializeField]
+    private string descriptionText;
+
+    private MeshRenderer meshRenderer;
+    private Collider collider;
+    private List<InventoryObject> playerInventory;
+
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public void DoActivate()
+    {
+        meshRenderer.enabled = false;
+        collider.enabled = false;
+        playerInventory.Add(this);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerInventory = FindObjectOfType<InventoryMenu>().PlayerInventory;
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+	}
+	
+}
